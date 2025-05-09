03d6a05fd0ccddc66e1eb137559f2c8d649b17c0
Fejoe Js <jsfejoe@gmail.com>
2025-05-09 17:36:08 +0530

    Add files via upload

---
+++ mavenproject3/src/main/java/com/yourcompany/Main/Main.java
@@ -0,0 +1,8 @@
+package com.yourcompany.Main;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println("Sum of 3 and 5 is: " + calculator.add(3, 5));
+    }
+}
